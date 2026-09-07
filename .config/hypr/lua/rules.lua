6c3aa2d01253defe43634beed8a2e132fb40c3a6
Buck <kbuck@tuta.io>
2026-09-07 18:03:23 +0300

    Add files via upload

--- .config/hypr/lua/rules.lua
+++ .config/hypr/lua/rules.lua
@@ -3,14 +3,14 @@
 -- └─┘└─┘┘└┘└┴┘└─┘┴└─┴ ┴└─┘
 -- https://github.com/kbuckleys/
 
-hl.window_rule({ match = { class = "org.quickshell", title = "terminus" },         float = true, size = { 1000, 1000 }})
-hl.window_rule({ match = { class = "org.quickshell", title = "terminus-picker" },  float = true, size = { 1000, 450 }})
-hl.window_rule({ match = { class = "steam", title = "Steam Settings" },         float = true })
-hl.window_rule({ match = { class = "kitty", title = "cynosure" },            float = true, size = { 1000, 1000 }})
-hl.window_rule({ match = { class = "kitty",title = "sysmon" },             float = true, size = { 1000, 1100 }})
-hl.window_rule({ match = { class = "kitty",title = "ZENU" },               float = true, size = { 1000, 1100 }})
-hl.window_rule({ match = { class = "swayimg" },                                 float = true })
-hl.window_rule({ match = { class = "mpv" },                                     float = true })
+hl.window_rule({ match = { class = "org.quickshell", title = "terminus" },         float = true })
+hl.window_rule({ match = { class = "org.quickshell", title = "terminus-picker" },  float = true })
+hl.window_rule({ match = { class = "steam", title = "Steam Settings" },            float = true })
+hl.window_rule({ match = { class = "kitty", title = "cynosure" },                  float = true, size = { 1000, 1000 }})
+hl.window_rule({ match = { class = "kitty",title = "sysmon" },                     float = true, size = { 1000, 1100 }})
+hl.window_rule({ match = { class = "kitty",title = "ZENU" },                       float = true, size = { 1000, 1100 }})
+hl.window_rule({ match = { class = "swayimg" },                                    float = true })
+hl.window_rule({ match = { class = "mpv" },                                        float = true })
 
 -- BORDERS
 hl.window_rule({ match = { fullscreen = true },  border_color = "#fab38799", border_size = 1, rounding = 5 })
