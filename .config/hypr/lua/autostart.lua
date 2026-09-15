c3b72347e3e12a56899cef956887635dd42ae5c7
Buck <kbuck@tuta.io>
2026-09-16 01:45:17 +0300

    Add files via upload

--- .config/hypr/lua/autostart.lua
+++ .config/hypr/lua/autostart.lua
@@ -4,7 +4,7 @@
 -- https://github.com/kbuckleys/
 
 hl.on("hyprland.start", function()
-    hl.exec_cmd("qs")
+    hl.exec_cmd("qs -d")
     hl.exec_cmd("wl-clip-persist --clipboard regular")
     hl.exec_cmd("dbus-update-activation-environment --all")
     hl.exec_cmd("systemctl --user import-environment")
